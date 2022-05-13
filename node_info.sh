13a27a8a48efbd4f821030ae8eb2e5a907fd4cf3
SecorD0 <vovan6573@gmail.com>
2022-05-13 03:12:54 +0300

    Shows node info

--- node_info.sh
+++ node_info.sh
@@ -53,44 +53,76 @@ main() {
 		local t_nn="\nНазвание ноды:          ${C_LGn}%s${RES}"
 		local t_nv="Версия ноды:            ${C_LGn}%s${RES}"
 		local t_lb="Последний блок:         ${C_LGn}%d${RES}"
-		local t_sy1="Нода синхронизирована:  ${C_LR}нет${RES}\n"
-		local t_sy2="Нода синхронизирована:  ${C_LGn}да${RES}\n"
+		local t_sy1="Нода синхронизирована:  ${C_LR}нет${RES}"
+		local t_sy2="Осталось нагнать:           ${C_LR}%d-%d=%d${RES}\n"
+		local t_sy3="Нода синхронизирована:  ${C_LGn}да${RES}\n"
 		
-		local t_m1="Майнер запущен:         ${C_LGn}да${RES}"
-		local t_m2="Майнер запущен:         ${C_LR}нет${RES}"
+		local t_m1="Майнер:                 ${C_LGn}пул${RES}"
+		local t_m2="Майнер:                 ${C_LGn}запущен${RES}"
+		local t_m3="Майнер:                 ${C_LR}не запущен${RES}\n"
 		local t_t="Потоков используется:   ${C_LGn}%d${RES}\n"
-		#local t_bm="Блоков намайнено:       ${C_LGn}%d${RES}\n"
 		
 		local t_wn="Название кошелька:      ${C_LGn}%s${RES}"
 		local t_wa="Адрес кошелька:         ${C_LGn}%s${RES}"
 		local t_bal="Баланс:                 ${C_LGn}%f${RES} IRON\n"
+		
+		local t_err_nwn="\n${C_R}Нода не работает!${RES}\n"
 
 	# Send Pull request with new texts to add a language - https://github.com/SecorD0/Iron_Fish/blob/main/node_info.sh
 	#elif [ "$language" = ".." ]; then
 	else
-		local t_nn="\nMoniker:               ${C_LGn}%s${RES}"
-		local t_nv="Node version:          ${C_LGn}%s${RES}"
-		local t_lb="Latest block height:   ${C_LGn}%d${RES}"
-		local t_sy1="Node is synchronized:  ${C_LR}no${RES}\n"
-		local t_sy2="Node is synchronized:  ${C_LGn}yes${RES}\n"
+		local t_nn="\nMoniker:                 ${C_LGn}%s${RES}"
+		local t_nv="Node version:            ${C_LGn}%s${RES}"
+		local t_lb="Latest block height:     ${C_LGn}%d${RES}"
+		local t_sy1="Node is synchronized:    ${C_LR}no${RES}"
+		local t_sy2="It remains to catch up:  ${C_LR}%d-%d=%d${RES}\n"
+		local t_sy3="Node is synchronized:    ${C_LGn}yes${RES}\n"
 		
-		local t_m1="Miner launched:        ${C_LGn}yes${RES}"
-		local t_m2="Miner launched:        ${C_LR}no${RES}"
-		local t_t="Threads are used:      ${C_LGn}%d${RES}\n"
-		#local t_bm="Blocks mined:          ${C_LGn}%d${RES}\n"
+		local t_m1="Miner:                   ${C_LGn}pool${RES}"
+		local t_m2="Miner:                   ${C_LGn}launched${RES}"
+		local t_m3="Miner:                   ${C_LR}not launched${RES}\n"
+		local t_t="Threads are used:        ${C_LGn}%d${RES}\n"
 		
-		local t_wn="Wallet name:           ${C_LGn}%s${RES}"		
-		local t_wa="Wallet address:        ${C_LGn}%s${RES}"
-		local t_bal="Balance:               ${C_LGn}%f${RES} IRON\n"		
+		local t_wn="Wallet name:             ${C_LGn}%s${RES}"		
+		local t_wa="Wallet address:          ${C_LGn}%s${RES}"
+		local t_bal="Balance:                 ${C_LGn}%f${RES} IRON\n"
+
+		local t_err_nwn="\n${C_R}Node isn't working!${RES}\n"		
 	fi
 	
 	# Actions
 	sudo apt install wget awk jq bc -y &>/dev/null
+	if ! docker ps -a | grep iron_fish_node | grep -q "Up" && ! sudo systemctl cat ironfishd 2>/dev/null | grep -q running; then
+		printf_n "$t_err_nwn"
+		return 1 2>/dev/null; exit 1
+	fi
+	
 	if docker ps -a | grep -q iron_fish_node; then
 		local command="docker exec -t iron_fish_node ironfish"
-		local threads=`docker inspect iron_fish_miner | jq -r ".[0].Config.Cmd[2]"`
+		local response=`docker inspect iron_fish_miner 2>&1`
+		if ! grep -q "No such" <<< "$response"; then
+			if grep -q "pool" <<< "$response"; then
+				local miner="pool"
+			else
+				local miner="true"
+			fi
+			local threads=`jq -r ".[0].Config.Cmd[-1]" <<< "$response"`
+		else
+			local miner="false"
+		fi
 	else
 		local command="ironfish"
+		local response=`sudo systemctl status ironfishd-miner | grep running`
+		if [ -n "$response" ]; then
+			if grep -q "pool" <<< "$response"; then
+				local miner="pool"
+			else
+				local miner="true"
+			fi
+			local threads=`sudo systemctl cat ironfishd-miner | grep -oPm1 "(?<=\-t )([^%]+)(?= \-\-no)"`
+		else
+			local miner="false"
+		fi
 	fi
 	
 	local moniker=`$command config:get nodeName --no-color | tr -d '"' | tr -d '\r'`
@@ -102,16 +134,11 @@ main() {
 	else
 		local catching_up="true"
 	fi
-	if [ `echo "$status" | grep Mining | awk '{print $4}'` -ge 1 ]; then
-		local miner="true"
-	else
-		local miner="false"
-	fi
-	#local blocks_mined=`echo "$status" | awk 'NR == 7 {print $6}'`
+	
 	local wallet_name=`$command accounts:which | tr -d '\r'`
 	local wallet_address=`$command accounts:publickey | tr -d '\r' | awk '{print $(NF)}'`
-	local balance=`$command accounts:balance | tr -d '\r' | awk 'NR == 1 {print $(NF-2)}'`
-
+	local balance=`$command accounts:balance | tr -d '\r' | grep "The balance is" | awk '{print $(NF-2)}'`
+	
 	# Output
 	if [ "$raw_output" = "true" ]; then
 		printf_n '{"moniker": "%s", "node_version": "%s", "latest_block_height": %d, "catching_up": %b, "miner": %b, "threads": %d, "blocks_mined": %d, "wallet_name": "%s", "wallet_address": "%s", "balance": %f}' \
@@ -130,19 +157,23 @@ main() {
 		printf_n "$t_nv" "$node_version"
 		printf_n "$t_lb" "$latest_block_height"
 		if [ "$catching_up" = "true" ]; then
-			printf_n "$t_sy1"	
+			local current_block=`wget -qO- "https://api-production.ironfish.network/blocks?limit=1&main=true" | jq -r ".data[0].sequence"`
+			local diff=`bc -l <<< "$current_block-$latest_block_height"`
+			printf_n "$t_sy1"
+			printf_n "$t_sy2" "$current_block" "$latest_block_height" "$diff"
 		else
-			printf_n "$t_sy2"
+			printf_n "$t_sy3"
 		fi
 		
-		if [ "$miner" = "true" ]; then
-			printf_n "$t_m1"	
-		else
+		if [ "$miner" = "pool" ]; then
+			printf_n "$t_m1"
+		elif [ "$miner" = "true" ]; then
 			printf_n "$t_m2"
+		else
+			printf_n "$t_m3"
 		fi
 		if [ -n "$threads" ]; then printf_n "$t_t" "$threads"; fi
-		#printf_n "$t_bm" "$blocks_mined"
-		
+
 		printf_n "$t_wn" "$wallet_name"
 		printf_n "$t_wa" "$wallet_address"
 		printf_n "$t_bal" "$balance" 2>/dev/null
